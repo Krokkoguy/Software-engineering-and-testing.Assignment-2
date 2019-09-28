270f872cf92d6c288952bfa3ceae74b129477dd3
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-09-28 22:26:52 +0200

    minor update

--- src/main/java/HiofFizzBuzz.java
+++ src/main/java/HiofFizzBuzz.java
@@ -1,4 +1,5 @@
 public class HiofFizzBuzz {
+    /* ## Deloppgave 1 ## */
     public static void main(String[] args){
         for (int i = 1; i <= 100; i++ ){
             System.out.println( parse(i) );
--- src/test/java/HiofTester.java
+++ src/test/java/HiofTester.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class HiofTester {
+    /* ## Deloppgave 2 ## */
     @ParameterizedTest
     /* Valgte her å bruke @CsvFileSource i stedet for @ValueSource */
-    @CsvFileSource(resources = "arguments.csv")
+    @CsvFileSource(resources = "arguments.csv") //IDE complains this file doesn't exist. Don't trust it :)
     void test(String expected, int value ) {
         assertEquals(expected, HiofFizzBuzz.parse(value));
     }
