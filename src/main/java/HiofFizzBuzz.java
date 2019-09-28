d28260db67ca0510b152da657b1c34d31938ca14
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-09-28 22:04:22 +0200

    Updated .parse to satisfy tests

--- src/main/java/HiofFizzBuzz.java
+++ src/main/java/HiofFizzBuzz.java
@@ -5,6 +5,7 @@ public class HiofFizzBuzz {
         }
     }
     public static String parse( int n ){
+        if( n%15 == 0 ) return "HiOf";
         if( n%3 == 0 ) return "Hi";
         if( n%5 == 0 ) return "Of";
         return String.valueOf(n);
