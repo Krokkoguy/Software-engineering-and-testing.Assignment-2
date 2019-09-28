6d2bcbab1b88061a8464ec4002d320b856808dbe
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-09-28 22:03:03 +0200

    Added tests
    They succeed

--- src/test/java/HiofTester.java
+++ src/test/java/HiofTester.java
@@ -11,7 +11,15 @@ public class HiofTester {
             "Hi, 3",
             "4, 4",
             "Of, 5",
-            "Hi, 6"
+            "Hi, 6",
+            "7, 7",
+            "8, 8",
+            "Hi, 9",
+            "Of, 5",
+            "11, 11",
+            "Hi, 12",
+            "13, 13",
+            "14, 14"
     })
     void test(String expected, int value ) {
         assertEquals(expected, HiofFizzBuzz.parse(value));
